f9948cd604b2823216bb35315d0da829074b4242
Tom Hinkle <tmhinkle@gmail.com>
2022-08-11 08:53:33 -0400

    Move schedule stuff to its own module

--- svelte/src/TimeDisplay.svelte
+++ svelte/src/TimeDisplay.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
-  import ScheduleBlockDisplay from "./Schedule/ScheduleBlockDisplay.svelte";
+  import ScheduleDisplay from "./Schedule/ScheduleDisplay.svelte";
+
   import { fly, fade } from "svelte/transition";
   import { onMount } from "svelte";
 
   import Menu from "./Menu.svelte";
-  import { getHSBlock, ScheduleBlock } from "./Schedule/schedule";
 
-  let today = new Date().toLocaleString("en-US", {
+  let now = new Date();
+  let today = now.toLocaleString("en-US", {
     weekday: "short",
     month: "short",
     day: "numeric",
@@ -14,16 +15,6 @@
     minute: "numeric",
     second: "numeric",
   });
-
-  let {
-    currentBlock,
-    previousBlock,
-    nextBlocks,
-  }: {
-    currentBlock: ScheduleBlock;
-    previousBlock: ScheduleBlock;
-    nextBlocks: ScheduleBlock[];
-  } = getHSBlock(new Date());
 
   onMount(() => {
     setInterval(() => {
@@ -36,10 +27,6 @@
         minute: "numeric",
         second: "numeric",
       });
-      let hsinfo = getHSBlock(now);
-      currentBlock = hsinfo.currentBlock;
-      nextBlocks = hsinfo.nextBlocks;
-      previousBlock = hsinfo.previousBlock;
     }, 1000);
   });
   let showAll;
@@ -47,46 +34,7 @@
 
 <div class="rows">
   <div class="time">{today}</div>
-  <div class="blocks">
-    <a
-      href="https://docs.google.com/document/d/1E5J9z_fa_PHb-xvwR1W0NvGeV1kGUbgAg-wcvg9bXIQ/edit"
-      >HS🔔</a
-    >
-    <!-- <div class="prev">
-      {#if previousBlock}
-        <ScheduleBlockDisplay block={previousBlock} />
-      {:else}
-        …{/if}
-    </div> -->
-    <div class="current">
-      {#if currentBlock}
-        <ScheduleBlockDisplay block={currentBlock} bold={true} />
-      {:else}…{/if}
-    </div>
-
-    {#each nextBlocks as nextBlock, i}
-      {#if showAll || i < 2}
-        <div
-          class={`next next-${i}`}
-          in:fly|local={{ x: 500 }}
-          out:fly|local={{ x: 500 }}
-        >
-          <ScheduleBlockDisplay block={nextBlock} />
-        </div>
-      {/if}
-    {:else}
-      <div class="next">…</div>
-    {/each}
-    {#if nextBlocks.length > 2}
-      <button on:click={() => (showAll = !showAll)}>
-        {#if showAll}
-          ◀
-        {:else}
-          ▶
-        {/if}
-      </button>
-    {/if}
-  </div>
+  <ScheduleDisplay {now} />
 </div>
 
 <style>
