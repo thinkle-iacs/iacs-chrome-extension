7cc02aba88762db43b9282769ab5a40437b578e3
Tom Hinkle <tmhinkle@gmail.com>
2023-03-22 10:37:00 -0400

    ++purple Thursday

--- svelte/src/whimsy/DayOfWeekWhimsy.svelte
+++ svelte/src/whimsy/DayOfWeekWhimsy.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">  
+  import PurpleThursday from "./PurpleThursday.svelte";
   import { now } from "../Schedule/now";
   let days = [
     'Sunday',
@@ -14,35 +15,12 @@
   $: dayNum = $now.getDay();
   
 </script>
-<p on:animationstart={()=>hide=false} class:hide>Today is {days[dayNum]}</p>
 
-
+{#if dayNum==4}
+<PurpleThursday/>
+{/if}
 
 <style>
-  p {
-    animation-name : zoom-in;
-    animation-duration: 1s;    
-    animation-delay: 2s;
-    animation-timing-function: ease-in-out;
-    transform-origin: center;
-    display: grid;
-    place-content: center;
-    font-size: 3rem;
-    text-align: center;   
-  }
-  @keyframes zoom-in {
-    0% {
-      transform: scale(0)translateY(100px);
-      opacity: 0;
-    }    
-    90% {
-      transform: scale(1.8)translateY(-10px)rotate(5deg);
-    }
-    100% {
-      transform: scale(1)translateY(0);
-    }
-  }
-  .hide {
-    opacity: 0;
-  }
+ 
+  
 </style>
