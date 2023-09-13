182f7d3cdc870cb45bc1664c1401036fb898c76a
Tom Hinkle <tmhinkle@gmail.com>
2023-09-13 06:56:11 -0400

    Control widths of numbers so our timer widget doesn't change between two and three lines as the time counts down.

--- svelte/src/whimsy/Countdown.svelte
+++ svelte/src/whimsy/Countdown.svelte
@@ -1,7 +1,7 @@
-<script lang="typescript">
-  export let target = new Date(2023,5,20,12,25);
-  export let countdownStart = new Date(2022,7,30,8,10)  
-  export let name = "Summer Vacation"
+<script lang="ts">
+  export let target = new Date(2023, 5, 20, 12, 25);
+  export let countdownStart = new Date(2022, 7, 30, 8, 10);
+  export let name = "Summer Vacation";
   import { now } from "../Schedule/now";
   import Hourglass from "./Hourglass.svelte";
   let targetMS = 0;
@@ -13,30 +13,30 @@
   $: updateCountdowns(remaining);
   var daysLeft = 0;
   var hoursLeft = 0;
-  var minutesLeft = 0;  
+  var minutesLeft = 0;
   var secondsLeft = 0;
-  let totalTime : number;
+  let totalTime: number;
   $: totalTime = targetMS - countdownStart.getTime();
   //$: console.log('Total time=',totalTime)
   //$: console.log('Remaining = ',remaining,'out of',totalTime,'elapsed=',totalTime-remaining)
   let percentage = 0;
-  
-  $: percentage = (totalTime-remaining) / totalTime
-  
-  function makeItNow () {
+
+  $: percentage = (totalTime - remaining) / totalTime;
+
+  function makeItNow() {
     target = new Date();
   }
 
-  function zeroPad (num) {
+  function zeroPad(num) {
     let s = `${num}`;
-    if (s.length==1) {
-      return '0'+s
+    if (s.length == 1) {
+      return "0" + s;
     } else {
       return s;
     }
   }
 
-  function updateCountdowns (ms) {
+  function updateCountdowns(ms) {
     const second = 1000;
     const minute = second * 60;
     const hour = minute * 60;
@@ -48,42 +48,47 @@
     remainder = remainder % hour;
     minutesLeft = Math.floor(remainder / minute);
     remainder = remainder % minute;
-    secondsLeft = Math.floor(remainder / second);            
+    secondsLeft = Math.floor(remainder / second);
   }
-  
-  
-
 </script>
+
 <div class="countdown">
-  <div class="icon"> 
-    {#key target}<Hourglass {percentage}/> {/key}
+  <div class="icon">
+    {#key target}<Hourglass {percentage} /> {/key}
   </div>
   <div class="words">
     {#if remaining > 0}
-  {#if daysLeft}
-    <span class="days">
-      {daysLeft} <span class="unit">days</span>
-    </span>
-  {/if} 
-  {#if hoursLeft}
-    <span class="hours">{hoursLeft} <span class="unit">hours</span></span>
-  {/if}
-  <span class="time">{zeroPad(minutesLeft)}:{zeroPad(secondsLeft)}</span>          
-  <span class="until">until <b class="target" on:dblclick={makeItNow}>{name}</b></span>
-  {:else}
-  We made it to <b class="target">{name}</b>
-  {/if}
-    <slot/>
+      {#if daysLeft}
+        <span class="days">
+          <span class="digits-3">{daysLeft}</span>
+          <span class="unit">days</span>
+        </span>
+      {/if}
+      {#if hoursLeft}
+        <span class="hours"
+          ><span class="digits-2">{hoursLeft}</span>
+          <span class="unit">hours</span></span
+        >
+      {/if}
+      <span class="time digits-4"
+        >{zeroPad(minutesLeft)}:{zeroPad(secondsLeft)}</span
+      >
+      <span class="until"
+        >until <b class="target" on:dblclick={makeItNow}>{name}</b></span
+      >
+    {:else}
+      We made it to <b class="target">{name}</b>
+    {/if}
+    <slot />
   </div>
-  
 </div>
 
 <style>
   .until {
     display: block;
   }
-  .countdown {    
-    padding: var(--pad);    
+  .countdown {
+    padding: var(--pad);
     --background-color: var(--blue);
     color: var(--black);
   }
@@ -94,9 +99,23 @@
     align-items: center;
   }
   .icon {
-    
   }
   .unit {
     font-size: small;
+  }
+  .digits-2 {
+    width: 1.3em;
+    display: inline-flex;
+    justify-content: end;
+  }
+  .digits-3 {
+    width: 1.8em;
+    display: inline-flex;
+    justify-content: end;
+  }
+  .digits-4 {
+    width: 2.67em;
+    display: inline-flex;
+    justify-content: end;
   }
 </style>
