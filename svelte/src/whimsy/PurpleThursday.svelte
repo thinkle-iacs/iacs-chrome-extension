2ae1bb690acb16c2d4c6604f6ea6b14f361ed861
Tom Hinkle <tmhinkle@gmail.com>
2025-04-09 11:07:21 -0400

    Fix purple thursday in dark mode

--- svelte/src/whimsy/PurpleThursday.svelte
+++ svelte/src/whimsy/PurpleThursday.svelte
@@ -1,36 +1,65 @@
 <script>
   import { onMount } from "svelte";
-  onMount(
-    ()=>{
-      
-    }
-  )
-
-  $: {
-    if (!hide && animating) {
-      document.documentElement.style.setProperty('--red','var(--material-color-purple-900)');
-      document.documentElement.style.setProperty('--blue','var(--material-color-purple-600)');
-      console.log('Set!',document.documentElement.style)
-    } else {
-      document.documentElement.style.setProperty('--red','');
-      document.documentElement.style.setProperty('--blue','');
-
-    }
-  }
 
   let hide = false;
   let animating = false;
+
+  // Function to update colors based on the current color scheme
+  function updateColors() {
+    const isDarkMode = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+
+    if (!hide && animating) {
+      document.documentElement.style.setProperty(
+        "--red",
+        isDarkMode
+          ? "var(--material-color-purple-100)"
+          : "var(--material-color-purple-900)"
+      );
+      document.documentElement.style.setProperty(
+        "--blue",
+        isDarkMode
+          ? "var(--material-color-purple-200)"
+          : "var(--material-color-purple-600)"
+      );
+    } else {
+      document.documentElement.style.setProperty("--red", "");
+      document.documentElement.style.setProperty("--blue", "");
+    }
+  }
+
+  // Watch for changes in the color scheme
+  onMount(() => {
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+    mediaQuery.addEventListener("change", updateColors);
+
+    // Initial call to set the colors
+    updateColors();
+
+    return () => {
+      mediaQuery.removeEventListener("change", updateColors);
+    };
+  });
+
+  $: updateColors(hide, animating);
 </script>
+
 {#if !hide}
-<div class:notAnimated={!animating} on:animationstart={()=>animating=true} on:click={()=>hide=true}>
-  It's purple Thursday
-</div>
+  <div
+    class:notAnimated={!animating}
+    on:animationstart={() => (animating = true)}
+    on:click={() => (hide = true)}
+  >
+    It's purple Thursday
+  </div>
 {/if}
+
 <style>
   .notAnimated {
     opacity: 0;
   }
-   div {
+  div {
     display: grid;
     place-content: center;
     text-align: center;
@@ -49,19 +78,18 @@
     animation-delay: 2s;
     animation-iteration-count: 1;
     z-index: 99;
-   }
+  }
 
-   @keyframes slide-in {
+  @keyframes slide-in {
     0% {
       right: -500px;
       bottom: 50vh;
-      transform: rotateX(45deg)rotate(5deg);
+      transform: rotateX(45deg) rotate(5deg);
     }
     100% {
       right: -100px;
       bottom: 50px;
       transform: rotate(-45deg);
     }
-
-   }
+  }
 </style>
