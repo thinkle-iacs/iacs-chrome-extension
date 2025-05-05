78e8a9da48617ae1a37cad5bab5869e733bf6d8f
Tom Hinkle <tmhinkle@gmail.com>
2025-05-05 12:49:48 -0400

    ++lunch menus for students

--- svelte/src/menus/studentMenus.ts
+++ svelte/src/menus/studentMenus.ts
@@ -21,6 +21,22 @@ import {
   STELLA,
 } from "./commonMenuItems";
 
+const FOOD = {
+  title: "Food",
+  items: [
+    {
+      title: "Lunch Menu",
+      icon: "./icons/lunch-menu.png",
+      link: "https://drive.google.com/drive/folders/0B-fhMzqaF6ywMDNpZXFqMFk1dUE?resourcekey=0-Rl9upCRc9k7TfJJxYY0Csg",
+    },
+    {
+      link: "https://www.innovationcharter.org/food-nutrition-services/",
+      title: "Lunch & Nutrition Page",
+      icon: "./icons/lunch.png",
+    },
+  ],
+};
+
 const studentapps = [ASPEN, GC];
 
 export const hsmenu = [
@@ -35,7 +51,7 @@ export const hsmenu = [
         title: "Adobe",
         items: [EXPRESS, CREATIVECLOUD],
       },
-      CLEVER
+      CLEVER,
     ],
   },
 
@@ -53,13 +69,11 @@ export const hsmenu = [
   {
     title: "",
     items: [
-    {title: "Resources",
-      items: [
-      HS_ANNOUNCEMENTS,
-      HS_HANDBOOK,
-      BULLYING_PLAN]
-    },
-    {
+      {
+        title: "Resources",
+        items: [HS_ANNOUNCEMENTS, HS_HANDBOOK, BULLYING_PLAN],
+      },
+      {
         title: "Forms",
         items: [
           CHROMEBOOK_REPAIR_FORM,
@@ -81,6 +95,7 @@ export const hsmenu = [
     ],
   },
   LINKS,
+  FOOD,
 ];
 
 export const msmenu = [
@@ -90,38 +105,38 @@ export const msmenu = [
   },
   {
     title: "",
-    items: [GOOGLE_APPS, 
+    items: [
+      GOOGLE_APPS,
       {
-        title : 'Other Apps',
-        items: [EXPRESS, CLEVER]
-      }]
+        title: "Other Apps",
+        items: [EXPRESS, CLEVER],
+      },
+    ],
   },
   {
-    title : "",
-    items : [
+    title: "",
+    items: [
       {
         title: "Resources",
+        items: [MS_HANDBOOK, BULLYING_PLAN],
+      },
+      {
+        title: "Forms",
         items: [
-          MS_HANDBOOK, 
-          BULLYING_PLAN]
-        },
-        {
-          title: "Forms",
-          items: [
-            CHROMEBOOK_REPAIR_FORM,
-            {
-              title: "Incident Reporting Form",
-              link: "https://docs.google.com/forms/d/e/1FAIpQLSe9qasAJbfEcJvF1wzXS4UWC3NSuX_IT0Ep_jwV7Qbr-zVYvQ/viewform",
-            },
-            {
-              title: "Counseling Check-In Form",
-              link: "https://forms.gle/Dy7h4yRReG7Etyxf7",
-              detail: "Counseling Check-In Form for Students",
-              icon: "https://img.icons8.com/external-flaticons-lineal-color-flat-icons/344/external-counseling-funeral-service-flaticons-lineal-color-flat-icons.png",
-            },
-            RESTORATIVE_FORM,
-          ],
-        },
+          CHROMEBOOK_REPAIR_FORM,
+          {
+            title: "Incident Reporting Form",
+            link: "https://docs.google.com/forms/d/e/1FAIpQLSe9qasAJbfEcJvF1wzXS4UWC3NSuX_IT0Ep_jwV7Qbr-zVYvQ/viewform",
+          },
+          {
+            title: "Counseling Check-In Form",
+            link: "https://forms.gle/Dy7h4yRReG7Etyxf7",
+            detail: "Counseling Check-In Form for Students",
+            icon: "https://img.icons8.com/external-flaticons-lineal-color-flat-icons/344/external-counseling-funeral-service-flaticons-lineal-color-flat-icons.png",
+          },
+          RESTORATIVE_FORM,
+        ],
+      },
     ],
   },
   {
@@ -139,4 +154,5 @@ export const msmenu = [
       },
     ],
   },
+  FOOD,
 ];
