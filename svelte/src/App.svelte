629e1ca5c8e69591cf6c329a0ee14d37248e353b
MJ-2009 <januskiewiczmason60@gmail.com>
2026-05-27 16:28:34 +0000

    Add FlowerCanvas component and integrate into routing

--- svelte/src/App.svelte
+++ svelte/src/App.svelte
@@ -2,6 +2,7 @@
   import Innovator from "./whimsy/Innovator.svelte";
   import HawkCanvas from "./whimsy/HawkCanvas.svelte";
   import CountdownCard from "./whimsy/CountdownCard.svelte";
+  import FlowerCanvas from "./whimsy/FlowerCanvas.svelte";
 
   import TipBuilder from "./TipBuilder.svelte";
   import Snow from "./whimsy/SnowCanvas.svelte";
@@ -39,6 +40,7 @@
   let routes = {
     "#tipbuilder": "tipbuilder",
     "#snow": "snow",
+    "#flower": "flower",
     "#ball": "ball",
     "#ball-abs": "ball-abs",
     "#ball-fix": "ball-fix",
@@ -82,6 +84,7 @@
 </script>
 
 {#if route == "snow"}<Snow />{/if}
+{#if route == "flower"}<FlowerCanvas />{/if}
 {#if route == "hawk"}<HawkCanvas />{/if}
 {#if route == "ball" || route == "ball-abs"}
   <BouncingBall mode="absolute" />
